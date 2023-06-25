88015b6e010595710727f346c7dd772141802fa0
Marco Lee <117403240+MarcoLe3@users.noreply.github.com>
2023-06-24 19:48:10 -0700

    Diet Tracker.py

--- Diet Tracker.py
+++ Diet Tracker.py
@@ -1,6 +1,6 @@
-#Ask user for their calories for breakfast, lunch, dinner, and snacks in between meals
-#Ask user how many calories did user burn from exercising and their bmr 
-#Calculates user lbs. and says how much they will gain, lose, or stay the same. 
+#Ask user for their calories for breakfast, lunch, dinner, and snacks in between meals this week
+#Ask user how many calories did user burn from exercising this week and their bmr 
+#Calculates user lbs. and says how much they will gain, lose, or stay the same. this week
 
 
 class Diet:
@@ -16,19 +16,19 @@ class Diet:
         deficit = self.bmr + self.exercise - (self.breakfast_calories + self.lunch_calories + self.dinner_calories + self.snack_calories)
         return deficit
     
-breakfast_calories = int(input("How many calories did you have for breakfast? "))
-lunch_calories = int(input("How many calories did you have for lunch? "))
-dinner_calories = int(input("How many calories did you have for dinner? "))
-snack_calories = int(input("How many calories did you have for snack between meals? "))
-exercise = int(input("How many calories did you burn from exercising/ working out? "))
+breakfast_calories = int(input("How many calories did you have for breakfast this week? "))
+lunch_calories = int(input("How many calories did you have for lunch this week? "))
+dinner_calories = int(input("How many calories did you have for dinner this week? "))
+snack_calories = int(input("How many calories did you have for snack between meals this week? "))
+exercise = int(input("How many calories did you burn from exercising/ working out this week? "))
 bmr = int(input("What is your basic metabolic rate? "))
 
 fitness = Diet(breakfast_calories, lunch_calories, dinner_calories, snack_calories, exercise, bmr)
 weekly_deficit = 7 * fitness.calorie_deficit()
 
 if weekly_deficit > 0:
-    print(f"You will lose {round(weekly_deficit/ 3600, 2)} lbs. per week ")
+    print(f"You've lost {round(weekly_deficit/ 3600, 2)} lbs. this week ")
 elif weekly_deficit == 0:
-    print("Your weight will stay the same. ")
+    print("Your weight will stay the same this week. ")
 else:
-    print(f"You will gain {round(-1 * weekly_deficit/ 3600, 2)} lbs. per week. ")
+    print(f"You've gain {round(-1 * weekly_deficit/ 3600, 2)} lbs. this week. ")
